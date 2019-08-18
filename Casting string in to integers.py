28c077e4fed112bb8f43f9d0e6e70429ba65ca4a
Tarak Nath sinha <taraksinha09@gmail.com>
2019-08-18 17:43:03 +0530

    python initial script

---
+++ Casting string in to integers.py
@@ -0,0 +1,9 @@
+#Casting string in to integers
+
+num1 = '100'
+num2 = '200'
+
+num1 = int(num1)
+num2 = int(num2)
+
+print(num1 + num2)
---
+++ Welcome Msg.py
@@ -0,0 +1,13 @@
+#Welcome Msg
+
+#msg = "It's me"
+
+#msg = msg.replace('me', 'Tarak')
+#print(msg)
+
+name = 'Hi'
+name1 = 'Tarak'
+
+#msg = '{}, {}. Welcome'.format(name, name1)
+#msg = f'{name},{name1.upper()}. Linux' 
+print(name)
